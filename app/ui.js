991440622cdfc71e5120d321212c2379177462bc
madhouse <hinkley.marvin@gmail.com>
2016-03-19 09:10:57 -0700

    ui selector abstraction

---
+++ app/ui.js
@@ -0,0 +1,19 @@
+var ui = {
+  show: function (element) {
+
+  },
+  hide: function (element) {
+
+  },
+  containers: {
+    content: document.querySelector('.content-container')
+  },
+  buttons: {
+    testButton: document.querySelector('.test-button')
+  },
+  inputs: {
+    testInput: document.querySelector('.test-input')
+  }
+};
+
+export default ui
